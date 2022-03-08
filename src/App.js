5a905859a5b90ec5f2be474bad9247e4170adadb
Brando Socarras <brandosocarras@gmail.com>
2022-03-08 11:47:48 -0600

    Can update database from website

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import CourseList from './components/CourseList';
 import { useData } from './utilities/firebase.js';
+import { timeParts } from './utilities/times.js';
 import './App.css';
 
 const schedule = {
@@ -29,19 +30,6 @@ const schedule = {
   }
 };
 
-const meetsPat = /^ *((?:M|Tu|W|Th|F)+) +(\d\d?):(\d\d) *[ -] *(\d\d?):(\d\d) *$/;
-
-const timeParts = meets => {
-  const [match, days, hh1, mm1, hh2, mm2] = meetsPat.exec(meets) || [];
-  return !match ? {} : {
-    days,
-    hours: {
-      start: hh1 * 60 + mm1 * 1,
-      end: hh2 * 60 + mm2 * 1
-    }
-  };
-};
-
 const mapValues = (fn, obj) => (
   Object.fromEntries(Object.entries(obj).map(([key, value]) => [key, fn(value)]))
 );
@@ -61,7 +49,7 @@ const Banner = ({ title }) => (
 );
 
 const App = () => {
-  const [schedule, loading, error] = useData('/', addScheduleTimes); 
+  const [schedule, loading, error] = useData('/', addScheduleTimes);
 
   if (error) return <h1>{error}</h1>;
   if (loading) return <h1>Loading the schedule...</h1>
--- src/components/Course.js
+++ src/components/Course.js
@@ -1,4 +1,5 @@
-import { hasConflict } from '../utilities/times.js';
+import { hasConflict, timeParts } from '../utilities/times.js';
+import { setData } from '../utilities/firebase.js';
 
 export const terms = { F: 'Fall', W: 'Winter', S: 'Spring'};
 
@@ -14,16 +15,36 @@ export const getCourseTerm = course => (
   terms[course.id.charAt(0)]
 );
 
+const getCourseMeetingData = course => {
+  const meets = prompt('Enter meeting data: MTuWThF hh:mm-hh:mm', course.meets);
+  const valid = !meets || timeParts(meets).days;
+  if (valid) return meets;
+  alert('Invalid meeting data');
+  return null;
+};
+
+const reschedule = async (course, meets) => {
+  if (meets && window.confirm(`Change ${course.id} to ${meets}?`)) {
+    try {
+      await setData(`/courses/${course.id}/meets`, meets);
+    } catch (error) {
+      alert(error);
+    }
+  }
+};
+
 const Course = ({ course, selected, setSelected }) => {
   const isSelected = selected.includes(course);
   const isDisabled = !isSelected && hasConflict(course, selected);
   const style = {
     backgroundColor: isDisabled? 'lightgrey' : isSelected ? 'lightgreen' : 'white'
   };
+
   return (
     <div className="card m-1 p-2"
-      style={style}
-      onClick={isDisabled ? null : () =>  setSelected(toggle(course, selected))}>
+        style={style}
+        onClick={isDisabled ? null : () =>  setSelected(toggle(course, selected))}
+        onDoubleClick={() => reschedule(course, getCourseMeetingData(course))}>
       <div className="card-body">
         <div className="card-title">{ getCourseTerm(course) } CS { getCourseNumber(course) }</div>
         <div className="card-text">{ course.title }</div>
